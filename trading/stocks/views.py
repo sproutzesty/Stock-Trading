a43fa12a04ea11f8d2997183fd0cf5334984d10d
Hannah Sinclair <hannahsinclair92@gmail.com>
2021-06-22 23:10:16 -0500

    commit changes

--- trading/stocks/views.py
+++ trading/stocks/views.py
@@ -1,7 +1,18 @@
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from .forms import TickerForm
 
 def index(request):
-    form = TickerForm()
+    if request.method == 'POST':
+        form = TickerForm(request.POST)
+        if form.is_valid():
+            ticker = request.POST['ticker']
+            return HttpResponseRedirect(ticker)
+    else:
+        form = TickerForm()
     return render(request, "stocks/index.html", {"form":form})
+
+def ticker(request, tid):
+    context = {}
+    context['ticker'] = tid
+    return render(request, "stocks/ticker.html", context)
